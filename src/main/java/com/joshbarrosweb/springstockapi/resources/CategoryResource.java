9eaca47d9b0bd4ce1e4e743d90f1d7c28f9ef509
Josue Barros <joshbarrosweb@gmail.com>
2021-12-30 20:43:34 -0300

    chore: added Category domain class

--- src/main/java/com/joshbarrosweb/springstockapi/resources/CategoryResource.java
+++ src/main/java/com/joshbarrosweb/springstockapi/resources/CategoryResource.java
@@ -3,13 +3,24 @@ package com.joshbarrosweb.springstockapi.resources;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.joshbarrosweb.springstockapi.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String index() {
-		return "Rest OK";
+	public List<Category> index() {
+		Category cat1 = new Category(1, "PHP");
+		Category cat2 = new Category(2, "JAVA");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 }
