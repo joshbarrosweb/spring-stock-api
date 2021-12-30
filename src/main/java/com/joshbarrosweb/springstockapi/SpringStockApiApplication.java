fb3aa205d3b671f5a55806c8dcadf703b2ceac46
Josue Barros <joshbarrosweb@gmail.com>
2021-12-30 20:19:56 -0300

    chore: first commit

---
+++ src/main/java/com/joshbarrosweb/springstockapi/SpringStockApiApplication.java
@@ -0,0 +1,13 @@
+package com.joshbarrosweb.springstockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStockApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joshbarrosweb/springstockapi/SpringStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshbarrosweb.springstockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
